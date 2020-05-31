12649140364e1723940108588abbbcf830cbcf76
yujin <ryz311@mail.ustc.edu.cn>
2020-05-31 11:36:23 +0800

    增加登录的异步请求功能

---
+++ cd-commom/src/main/java/com/ryz/crowdfunding/util/AjaxResult.java
@@ -0,0 +1,23 @@
+package com.ryz.crowdfunding.util;
+
+public class AjaxResult {
+
+    private boolean success;
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
